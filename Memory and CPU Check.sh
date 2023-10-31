a6b2cefa579ce63e98001372bc386b278b7a7411
p4pvn <91900183+p4pvn@users.noreply.github.com>
2023-10-31 21:53:38 +0530

    Update Memory and CPU Check.sh
    Added Time Interval of 60 seconds and Few comments.

--- Memory and CPU Check.sh
+++ Memory and CPU Check.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 # Set the thresholds for CPU and memory usage (in percentage)
-CPU_THRESHOLD=80
-MEMORY_THRESHOLD=80
+CPU_THRESHOLD=80        # Declaring variables and Assigning values
+MEMORY_THRESHOLD=80     # Declaring variables and Assigning values
 
 # Function to check CPU usage
 check_cpu_usage() {
@@ -21,3 +21,9 @@ check_memory_usage() {
         # You can add alert/notification mechanisms here, like sending an email or displaying a message.
     fi
 }
+
+while true; do
+    check_cpu_usage
+    check_memory_usage
+    sleep 60  # It will sleep for 60 seconds once checked and will check again after 60 seconds.
+done
