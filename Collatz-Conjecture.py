1c822c3031eee908022c03e46ae1d73f5688a732
p4pvn <91900183+p4pvn@users.noreply.github.com>
2023-11-11 22:37:50 +0530

    Update Collatz-Conjecture.py

--- Collatz-Conjecture.py
+++ Collatz-Conjecture.py
@@ -1,8 +1,8 @@
 n = int(input("Enter an Integer:"))
 while n != 1:
-    if n % 2 == 0:
-        n = n / 2
+    if n % 2 == 0:        #when n is even
+        n = n / 2         #divide it by 2
         print(int(n))
-    else:
-        n = 3 * n + 1
-        print(int(n))
+    else:                   #when n is not even, means odd
+        n = 3 * n + 1       #multiply it by 3 and add 1
+        print(int(n))     
